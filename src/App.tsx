730eafb37bbd6bb8c788bb22e1e9d45c36f55f3d
Кирилл <94886658+Kiri1111@users.noreply.github.com>
2022-10-27 22:21:54 +0300

    06 lesson useState

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Accordion from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
+import {OnOff} from "./OnOff/OnOff";
 
 function App() {
     console.log("App rendered")
@@ -12,11 +13,13 @@ function App() {
             <Accordion titleValue={'Users'} collapsed={false}/>
 
             <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={2}/>*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
+            <OnOff/>
+
         </div>
     );
 }
---
+++ src/OnOff/OnOff.tsx
@@ -0,0 +1,57 @@
+import {useState} from "react";
+
+type PropsType = {
+    // on: boolean
+}
+
+
+export function OnOff(props: PropsType) {
+
+    //let on = false
+    let [on, setOn] = useState<boolean>(false);
+
+    const onStyle = {
+        width: '30px',
+        height: '20px',
+        border: '1px solid black',
+        display: 'inline-block',
+        marginLeft: '30px',
+        padding: '3px',
+        backgroundColor: on ? 'green' : 'white'
+
+    }
+    const offStyle = {
+        width: '30px',
+        height: '20px',
+        border: '1px solid black',
+        display: 'inline-block',
+        padding: '3px',
+        marginLeft: '3px',
+        backgroundColor: on ? 'white' : 'red'
+
+    }
+    const indicatorStyle = {
+        width: '50px',
+        height: '50px',
+        borderRadius: '50px',
+        border: '7px solid black',
+        display: 'inline-block',
+        margin: '5px',
+        backgroundColor: on ? 'green' : 'red'
+    }
+    return <div>
+        <div style={onStyle} onClick={() => {
+            setOn(true)
+        }}>On
+        </div>
+        <div style={offStyle} onClick={() => {
+            setOn(false)
+        }}>Off
+        </div>
+        <div onClick={() => {
+
+        }} style={indicatorStyle}></div>
+    </div>
+
+
+}
